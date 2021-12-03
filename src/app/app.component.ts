8dd19d243e57b1f7603f4d5225a7a177a3a0204c
Brad Campbell <bfcampbell88@gmail.com>
2021-12-03 15:58:46 -0500

    first part of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gened1004-cf-project';
+  tabTitle = 'GENED 1004 - CF'
+
+  // Used to set a new tab title if needed
+  public constructor(private titleService: Title) { }
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
